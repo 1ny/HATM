ec4c826153063a0bc7308aafae82767885e1eeb6
yerin <1ny@github.com>
2025-04-09 00:59:01 +0900

    삼성 2024 하반기 오전 1번 문제 기출 풀이 완료

---
+++ CodingTest/Baekjoon/Samsung_SW/Project1/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(void) {
+
+    set<int> s;
+
+    s.insert(1);
+    s.insert(200);
+    s.insert(-1);
+    s.insert(3);
+
+    set<int>::iterator iter;
+    for (iter = s.begin(); iter != s.end(); iter++)
+        printf("%d ", *iter);
+    printf("\n");
+
+    s.erase(200);
+    s.clear();
+
+    set<int>::reverse_iterator riter;
+    for (riter = s.rbegin(); riter != s.rend(); riter++) {
+        printf("%d ", *riter);
+    }
+
+    return 0;
+}
